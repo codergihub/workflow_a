e2df9248cfea2ac4669b9db6ac3663535573c93b
webapis <webapis.github@gmail.com>
2021-10-27 08:55:15 +0500

    v

---
+++ workflow.config.js
@@ -0,0 +1,7 @@
+export default {
+    auth: {
+        google: { scope: 'sample google scope' },
+        dropbox: { scope: 'sample dropbox scope' },
+        facebook: { scope: 'sample facebook scope' }
+    }
+}
